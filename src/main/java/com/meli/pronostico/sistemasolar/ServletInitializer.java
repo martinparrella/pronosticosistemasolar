d876b6763b7d279a603706123bbcd22249353a68
Martin Parrella <martin.parrella@gmail.com>
2019-07-25 00:53:33 -0300

    Inicio proyecto examen meli

---
+++ src/main/java/com/meli/pronostico/sistemasolar/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.meli.pronostico.sistemasolar;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PronosticosistemasolarApplication.class);
+	}
+
+}

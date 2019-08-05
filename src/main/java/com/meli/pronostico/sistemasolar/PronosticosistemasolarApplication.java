c6c0ac41e61270adf5189d6888b47ae5413869a2
Martin Parrella <martin.parrella@gmail.com>
2019-08-05 00:11:35 -0300

    Refactoring de algunas consultas. Agregue casos de testing. Correccion y mejoras en el README

--- src/main/java/com/meli/pronostico/sistemasolar/PronosticosistemasolarApplication.java
+++ src/main/java/com/meli/pronostico/sistemasolar/PronosticosistemasolarApplication.java
@@ -1,13 +1,9 @@
 package com.meli.pronostico.sistemasolar;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import com.meli.pronostico.sistemasolar.repositories.IPronosticoClimaRepository;
 
 @SpringBootApplication
 public class PronosticosistemasolarApplication {
@@ -21,26 +17,4 @@ public class PronosticosistemasolarApplication {
         dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
 	}
 
-	@Bean
-	public CommandLineRunner pronosticos(IPronosticoClimaRepository pronosticoClimaRepository) {
-		return (args) -> {
-			//LOGGER.error("global: " + global.getCiclo());
-		};
-
-	}
-
 }
-
-//@RestController
-//class InitController {
-//	@GetMapping("/")
-//	@ResponseBody
-//	public String inicio() {
-//		StringBuilder mensaje = new StringBuilder();
-//		mensaje.append(
-//				"Sistema informático encargado de pronosticar el clima en el sistema solar de una galaxia lejana. ");
-//
-//		return mensaje.toString();
-//		
-//	}
-//}
--- src/main/java/com/meli/pronostico/sistemasolar/repositories/IPronosticoClimaRepository.java
+++ src/main/java/com/meli/pronostico/sistemasolar/repositories/IPronosticoClimaRepository.java
@@ -17,63 +17,66 @@ import com.meli.pronostico.sistemasolar.entities.PronosticoClima;
  */
 @Repository
 public interface IPronosticoClimaRepository extends JpaRepository<PronosticoClima, Long> {
-    
-	/**
-     * @param dia
-     * @return Devuelve el pronostico de ese dia
-     */
-    public PronosticoClima findByDiaEntero(int dia);
-    
-    /**
-     * @param dia
-     * @return Devuelve la variacion del pronostico de ese dia
-     */
-    //public List<PronosticoClima> findDistinctClimaByDiaEntero(int dia);
 
-    //@Query(value = "SELECT * FROM PRONOSTICO_CLIMA WHERE diaEntero = ?1", nativeQuery = true)
-    //@Query("select p from PronosticoClima p where p.diaEntero = :dia group by p.clima")
-    @Query("select p from PronosticoClima p where p.diaEntero = :dia")
-    public List<PronosticoClima> findDistinctClimaByDiaEntero(@Param("dia") int diaEntero);
-    
-    /**
-     * @param clima
-     * @return Devuelve la lista de dias con un mismo clima en un ciclo
-     */
+	/**
+	 * @param dia
+	 * @return Devuelve el pronostico de ese dia
+	 */
+	public PronosticoClima findByDiaEntero(int dia);
+
+	/**
+	 * @param dia
+	 * @return Devuelve la variacion del pronostico de ese dia
+	 */
+	@Query("select p from PronosticoClima p where p.diaEntero = :dia")
+	public List<PronosticoClima> findDistinctClimaByDiaEntero(@Param("dia") int diaEntero);
+
+	/**
+	 * @param clima
+	 * @return Devuelve la lista de dias con un mismo clima en un ciclo
+	 */
 	public List<PronosticoClima> findByClima(String clima);
 
-    /**
-     * @param diasExtra
-     * @return Devuelve la lista de pronosticos menor a diasExtra where x.diaEntero < ?1
-     */
+	/**
+	 * @param diasExtra
+	 * @return Devuelve la lista de pronosticos menor a diasExtra where x.diaEntero
+	 *         < ?1
+	 */
 	public List<PronosticoClima> findByDiaEnteroLessThan(int diasExtra);
-	
-    /**
-    * @return devuelve el pronostico de lluvia con mayor perimetro
-    */
-   public PronosticoClima findFirstByOrderByPerimetroDesc();
 
-   
-   /**
-   * @return devuelve el top de pronostico de lluvia con mayor perimetro
-   */
-  public PronosticoClima findTopByOrderByPerimetroDesc();
-  
-  /**
-   * @return devuelve el top de pronostico de lluvia con mayor perimetro
-   */
-  public List<PronosticoClima> findFirst10ByClima(String clima, Sort perimetro);
-   
-   
-  @Query(value = "SELECT * FROM PRONOSTICO_CLIMA", nativeQuery = true)
-  public List<PronosticoClima> findAllNativo(int diaEntero);
-  
-  
-  @Query("select new com.meli.pronostico.sistemasolar.dto.PeriodoClimaDTO(p.clima, count(p)) from PronosticoClima p group by p.clima, p.diaEntero")
-  public List<PeriodoClimaDTO> findClimaCount();
-  
-  @Query("select new com.meli.pronostico.sistemasolar.dto.PeriodoClimaDTO(p.clima, count(distinct p.diaEntero)) from PronosticoClima p group by p.clima")
-  public List<PeriodoClimaDTO>findAllDistinctDiaGroupByClima();
-  
-  @Query("select new com.meli.pronostico.sistemasolar.dto.PeriodoClimaDTO(p.clima, count(distinct p.diaEntero)) from PronosticoClima p where p.diaEntero < :dia group by p.clima")
-  public List<PeriodoClimaDTO> findDistinctDiaGroupByClimaWhereDiaEnteroLessThan(@Param("dia") int diasExtra);
+	/**
+	 * @return devuelve el pronostico de lluvia con mayor perimetro
+	 */
+	public PronosticoClima findFirstByOrderByPerimetroDesc();
+
+	/**
+	 * @return devuelve el top de pronostico de lluvia con mayor perimetro
+	 */
+	public PronosticoClima findTopByOrderByPerimetroDesc();
+
+	/**
+	 * @return devuelve el top de pronostico de lluvia con mayor perimetro
+	 */
+	public List<PronosticoClima> findFirst10ByClima(String clima, Sort perimetro);
+
+	/**
+	 * @param perimetro
+	 * @return Devuelve la lista de pronosticos que tengan el perimetro
+	 */
+	public List<PronosticoClima> findByPerimetro(double perimetro);
+	
+	/**
+	 * @return devuelve la cantidad de dias distintos x clima
+	 */
+	@Query("select new com.meli.pronostico.sistemasolar.dto.PeriodoClimaDTO(p.clima, count(distinct p.diaEntero)) from PronosticoClima p group by p.clima")
+	public List<PeriodoClimaDTO> findAllDistinctDiaGroupByClima();
+
+	/**
+	 * @return devuelve la cantidad de dias distintos que hay x clima, siendo el dia menor al pasado por parametro.
+	 */
+	@Query("select new com.meli.pronostico.sistemasolar.dto.PeriodoClimaDTO(p.clima, count(distinct p.diaEntero)) from PronosticoClima p where p.diaEntero < :dia group by p.clima")
+	public List<PeriodoClimaDTO> findDistinctDiaGroupByClimaWhereDiaEnteroLessThan(@Param("dia") int diasExtra);
+
+	@Query(value = "SELECT * FROM PRONOSTICO_CLIMA", nativeQuery = true)
+	public List<PronosticoClima> findAllNativo(int diaEntero);
 }
